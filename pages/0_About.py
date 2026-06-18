5f86cfa25bcd7cbb70f8b7985f436e890d5c4738
Harsh Mishra <harshmishra06102004@gmail.com>
2026-06-18 12:28:30 +0530

    Create 0_About.py

---
+++ pages/0_About.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("About")
+
+st.write(
+    """
+    CRISPRbact Streamlit Interface
+
+    This application provides a browser-based interface for CRISPRbact.
+
+    Original CRISPRbact software:
+    https://gitlab.pasteur.fr/dbikard/crisprbact
+
+    Developed by:
+    Bikard Lab, Institut Pasteur
+
+    Streamlit interface:
+    Harsh Mishra
+    IISER Bhopal
+    """
+)
